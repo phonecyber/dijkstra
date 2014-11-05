138b851b0b4845c837594678dfe800d945a63117
Phonecyber 'Hack <webmaster@phonecyber.net>
2014-11-05 18:50:33 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,20 +4,21 @@ using namespace std;
 
 void main()
 {
-	int numNode;
-	cout << "How many nodes: ";
-	cin >> numNode;
-
-	cout << endl;
-
+	start(); // start with selecting menu and input the graph
+	
 	Graph mygraph(numNode); // create a Graph's object called 'mygraph'
 	mygraph.init(); // initialize a graph
-	mygraph.create_matrix(); // ask user to input the whole weight, then create an adjacency matrix
+
+	if (menu == 1)
+		mygraph.create_matrix(); // ask user to input the whole weight, then create an adjacency matrix
+	else if (menu == 2)
+		mygraph.create_matrix_withValue(example); // select graph from example (1-5)
+
 	mygraph.create_list(); // convert the matrix to an adjacency linked list
 	mygraph.print_matrix(); // print the matrix
 	mygraph.print_list(); // print the linked list
 	mygraph.check_graph(); // check the graph to answer questions including output results
-	mygraph.shortest_path('A'); // show dijkstra's table, which starts from node 'A'
+	mygraph.shortest_path(); // implement dijkstra's algorithm
 
 	cout << endl;
 }
